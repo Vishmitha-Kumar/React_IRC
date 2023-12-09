7e7f4eeb30919952a5aa94a1a8fc92358ac21dcb
Vishmitha-Kumar <vishmithak05@gmail.com>
2023-12-09 16:31:05 +0530

    Day2

---
+++ Day2/sampleapp/src/App.js
@@ -0,0 +1,30 @@
+import './Assets/css/style.css'
+
+function App(){
+    return(
+        <div>
+             <div class="register">
+        <form  onsubmit="register()">
+            
+            <h3>Registration Form</h3>
+        
+        <input type="text" id="user" placeholder="Username" className="auth-input" required/>
+        
+        <input type="email"  placeholder="Email"className="auth-input" required/>
+        
+        <input type="password"  id="pwd" placeholder="Password" className="auth-input"required/>
+        <input type="password"  id="cpwd" placeholder="Conformpassword" className="auth-input"required/>
+     
+        <input type="tel" placeholder="Phone" className="auth-input"/>
+        
+        <input type="date" placeholder="DOB" className="auth-input"/>
+        
+        <input type="submit" value="Register" className="auth-btn"/>
+        
+    </form> 
+    
+</div> 
+        </div>
+    )
+}
+export default App
