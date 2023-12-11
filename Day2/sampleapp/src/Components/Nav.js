4be11c1522746ff61caea4b02c8c65960703ee24
Vishmitha-Kumar <vishmithak05@gmail.com>
2023-12-11 09:02:17 +0530

    Day2

---
+++ Day2/sampleapp/src/Components/Nav.js
@@ -0,0 +1,14 @@
+function Nav()
+{
+     return(
+        <div>
+        <div class="navbar">
+        <ul>
+        <li><a href="Nav.js">Register</a></li>
+         <li><a href="login.js">Login</a></li>
+         </ul>
+        </div>
+        </div>
+     )
+    }
+    export default Nav 
---
+++ Day2/sampleapp/src/Components/login.js
@@ -0,0 +1,22 @@
+function login() {
+    return (
+        <div>
+            <div class="register">
+                <form onsubmit="register()">
+
+                    <h3>Login</h3>
+
+                    <input type="text" id="user" placeholder="Username" class="auth-input" required/>
+
+                        <input type="email" placeholder="Email" class="auth-input" required/>
+
+                            <input type="password" id="pwd" placeholder="password" class="auth-input" required/>
+                                <input type="submit" value="Login" class="auth-btn"/>
+
+
+                                </form>
+                            </div>
+                        </div>
+                        )
+}
+export default login 
