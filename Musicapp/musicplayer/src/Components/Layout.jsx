cc86b9505ce66cc8c846684d9f22719f07af0726
Vishmitha-Kumar <vishmithak05@gmail.com>
2024-01-02 17:31:27 +0530

    Musicapp

---
+++ Musicapp/musicplayer/src/Components/Layout.jsx
@@ -0,0 +1,57 @@
+
+import '../assets/css/layout.css'
+
+const Layout = () => {
+    return (
+        <>
+            <div className="body">
+                <div className="navt">
+
+                </div>
+                <div className="navs">
+                    <div className="home">
+                        <div className="navdiv">
+                            <img className="img" src="" />
+                            <div className="name">
+                            </div>
+                        </div>
+                        <div className="navdiv">
+                            <img className="img" src="" />
+                            <div className="name">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="navdiv">
+                        <img className="img" src="" />
+                        <div className="name">
+                        </div>
+                    </div>
+                    <div className="navdiv">
+                        <img className="img" src="" />
+                        <div className="name">
+
+                        </div>
+                    </div>
+                    <div className="navdiv">
+                        <img className="img" src="" />
+                        <div className="name">
+
+                        </div>
+                    </div>
+                    <div className="navdiv">
+                        <img className="img" src="" />
+                        <div className="name">
+
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </>
+    )
+}
+
+export default Layout;
